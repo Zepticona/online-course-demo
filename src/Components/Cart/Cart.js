4b61fbea5dd1df5d3e57d733bfe324eae06f2242
Zepticona <67518532+Zepticona@users.noreply.github.com>
2020-08-27 21:23:39 +0600

    ready for upload

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './cart.css'
 
 const Cart = (props) => {
     
     const totalPrice = props.cart.reduce( (accumulator, course) => accumulator + course.price, 0);
     return (
-        <div>
-            <p>Courses Enrolled: {props.cart.length} </p>
-            <p>Total Price: ${totalPrice}</p>
+        <div className="cart-wrapper">
+            <h4>Courses Enrolled: {props.cart.length} </h4>
+            <h4>Total Price: ${totalPrice}</h4>
         </div>
     );
 };
